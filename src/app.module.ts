24a1d019faaf83ab7ef19d43c8264d96ec77b456
ongdv <ongdv@pusan.ac.kr>
2023-06-02 10:49:48 +0900

    [UPDATE] Update Skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import { DbModule } from './db/db.module';
+import { DbService } from './db/db.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '..', '.env'),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [DbModule],
+      useClass: DbService,
+      inject: [DbService],
+    }),
+    DbModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,67 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { json, urlencoded } from 'express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+const whitelist = [
+  'http://localhost:3000',
+  'https://41room.islab.dev',
+  'https://api-41room.islab.dev',
+];
+
+const dirList = [
+  'uploads',
+  'uploads/equipment',
+  'uploads/users',
+  'uploads/trace',
+];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(process.env.NODE_ENV);
+  const cors: CorsOptions | boolean =
+    process.env.NODE_ENV === 'development'
+      ? {
+          origin: '*',
+          methods: ['POST', 'PUT', 'DELETE', 'GET'],
+          credentials: true,
+        }
+      : {
+          origin: whitelist,
+          methods: ['POST', 'PUT', 'DELETE', 'GET'],
+          credentials: true,
+        };
+
+  console.log(cors);
+  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors(cors);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.use(json({ limit: '500mb' }));
+  app.use(urlencoded({ limit: '500mb', extended: true }));
+
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  dirList.forEach((item) => {
+    if (!fs.existsSync(item)) {
+      // uploads 폴더가 존재하지 않을시, 생성합니다.
+      fs.mkdirSync(item);
+    }
+  });
+
+  await app.listen(port);
 }
 bootstrap();
